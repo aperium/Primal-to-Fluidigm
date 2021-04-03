34b2d1b3ef261c60ff97b99f9dd13de975249ebb
Daniel R Williams <aperium@gmail.com>
2021-04-03 12:59:51 -0400

    corrected var names in analyze coverage

---
+++ primalscheme/analyzecoverage2.R
@@ -0,0 +1,39 @@
+
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyverse, magrittr, fs)
+
+# reqpacks <- c("tidyverse","magrittr","openxlsx","fs")
+# packstoinstall <- setdiff(reqpacks,installed.packages()[,1])
+# if(length(packstoinstall) > 0) install.packages(packstoinstall)
+# 
+# library(tidyverse)
+# library(magrittr)
+# library(openxlsx)
+# library(fs)
+
+# set working directory
+# getwd()
+# setwd("/fs/scratch/PAS1755/drw_wd/reduced_primal_by_clustal/primalscheme")
+setwd("/Users/aperium/Documents/GitHub/Primal-to-Fluidigm/primalscheme")
+
+# read csv file
+coverage <- path("coverage2.csv") %>%
+  read_csv(col_names = TRUE) %>%
+  mutate(reference = as.factor(reference),
+         percent_coverage = percent_coverage / 100)
+
+
+plots <- coverage %>%
+  ggplot(aes(config_target_overlap, percent_coverage)) +
+  geom_smooth() +
+  geom_point() + 
+  facet_wrap(vars(reference)) +
+  theme_minimal()
+
+png(file = "coverage_by_overlap2.png", width = 1000, height = 1000)
+plots
+dev.off()
+
+
+
+
