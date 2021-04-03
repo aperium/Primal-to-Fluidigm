baed37f2d76f60261607194b8465d062996c6f2a
Daniel R Williams <aperium@gmail.com>
2021-04-03 00:21:41 -0400

    created an alternative way to call primalscheme

---
+++ primalscheme/runprimalscheme2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# These are determined by PCR requirements. FLuidigm has strict parameters. Illumina MiSeq is less picky.
+AMPMIN=180
+AMPMAX=500
+
+# This fastas named in the list with specified overlaps
+LIST=$(ls fastas | awk 'BEGIN {FS = "."}{ORS = " "} {print $1}')
+OVERLAPS=( 40 45 50 55 60 65 70 )
+for OVERLAP in ${OVERLAPS[@]}; do mkdir overlap_$OVERLAP; for GENE in ${LIST[@]}; do primalscheme multiplex -a $AMPMIN -a $AMPMAX -n $GENE -t $OVERLAP -o overlap_$OVERLAP/$GENE -f fastas/$GENE.fasta; done; done
+
+exit
+
