373dcbf4f0868d3109158c51f59a59120fe5bc86
Daniel R Williams <aperium@gmail.com>
2021-04-03 11:06:36 -0400

    runnign longer overlaps

--- primalscheme/runprimalscheme2.sh
+++ primalscheme/runprimalscheme2.sh
@@ -6,7 +6,8 @@ AMPMAX=500
 
 # This fastas named in the list with specified overlaps
 LIST=$(ls fastas | awk 'BEGIN {FS = "."}{ORS = " "} {print $1}')
-OVERLAPS=( 40 45 50 55 60 65 70 )
+# OVERLAPS=( 40 45 50 55 60 65 70 )
+OVERLAPS=(75 80 85 90)
 for OVERLAP in ${OVERLAPS[@]}; do mkdir overlap_$OVERLAP; for GENE in ${LIST[@]}; do primalscheme multiplex -a $AMPMIN -a $AMPMAX -n $GENE -t $OVERLAP -o overlap_$OVERLAP/$GENE -f fastas/$GENE.fasta; done; done
 
 exit
