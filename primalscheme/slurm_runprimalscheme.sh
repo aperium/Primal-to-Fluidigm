2c103457976b5ef944e75a893e7b9cb6edb7652d
Daniel R Williams <aperium@gmail.com>
2021-04-04 17:06:51 -0400

    realized I messed up a list and need to re-run prmalscheme

--- primalscheme/slurm_runprimalscheme.sh
+++ primalscheme/slurm_runprimalscheme.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #SBATCH --account=PAS1755
-#SBATCH --time=130
+#SBATCH --time=224
 #SBATCH --output=slurm-runprimalscheme-%j.out
 set -e -u -o pipefail
 
 # execution takes about 17 min for each pass through the 106 fastas
 # first time running with the full set of 7 overlaps * 106 fastas took 116 minutes
+# set clocktime for 147*11*1.2 for a 20% margin of error in the calculation
 
 # move to directory
 cd /fs/scratch/PAS1755/drw_wd/Primal-to-Fluidigm/primalscheme
