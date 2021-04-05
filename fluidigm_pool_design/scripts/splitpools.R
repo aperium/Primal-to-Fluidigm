67b8f6fe90c3ff2578e32684436e0a905dd036b8
Daniel R Williams <aperium@gmail.com>
2021-04-05 12:26:56 -0400

    created script for Kruskal's algorithm.

---
+++ fluidigm_pool_design/scripts/splitpools.R
@@ -0,0 +1,14 @@
+
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyverse, magrittr, fs)
+
+path <- path("../out/pools/clustalout")
+files <- c("pool1.pim.csv", "pool2.pim.csv")
+
+
+for (i in 1:length(files)) {
+  # read ith file
+  edgeweights_i <- read_csv(path(path, files[i]), col_names = TRUE)
+  
+  
+}
