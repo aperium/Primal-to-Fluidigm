abba4b486fe7a2019e55a97461adad3a377bd729
Daniel R Williams <aperium@gmail.com>
2021-04-05 13:08:26 -0400

    specified col types

--- fluidigm_pool_design/scripts/splitpools.R
+++ fluidigm_pool_design/scripts/splitpools.R
@@ -8,7 +8,7 @@ files <- c("pool1.pim.csv", "pool2.pim.csv")
 
 for (i in 1:length(files)) {
   # read ith file
-  edgeweights_i <- read_csv(path(path, files[i]), col_names = TRUE)
+  edgeweights_i <- read_csv(path(path, files[i]), col_names = TRUE, col_types = "ccd")
   
   
 }
